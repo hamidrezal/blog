7557641532f6faeab35f09b4593a5171e737e668
HannyaUserGit <phoofic@gmail.com>
2024-06-12 13:18:33 +0330

    Add initial template with header and footer components

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+# Components
+    #1- Footer Components
+
+
+class HomeScreenView(View):
+    def get(self, request):
+        return render(request, 'blog/index.html')
+
+def header_component(request):
+    return render(request, 'blog/components/header_component.html')
+
+# Footer Components
+def footer_component(request):
+    return render(request,'blog/components/footer_component.html')
